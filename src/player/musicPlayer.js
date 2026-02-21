cb282b6fa992318d9ba7a2133ecdae924c2df4a0
Kartik <45144906+OfficialKartik@users.noreply.github.com>
2026-02-21 18:07:36 +0530

    Update musicPlayer.js

--- src/player/musicPlayer.js
+++ src/player/musicPlayer.js
@@ -12,13 +12,29 @@ class MusicPlayer {
   constructor() {
     this.connection = null;
     this.player = createAudioPlayer();
+    this.currentProcess = null;
   }
 
-  async play(message, url) {
+  safeDestroy() {
+    if (this.currentProcess) {
+      this.currentProcess.kill('SIGKILL');
+      this.currentProcess = null;
+    }
+
+    if (this.connection && this.connection.state.status !== 'destroyed') {
+      this.connection.destroy();
+      this.connection = null;
+    }
+  }
+
+  async play(message, query) {
     const channel = message.member.voice.channel;
     if (!channel) {
       return message.reply('Join VC first.');
     }
+
+    // Stop any existing playback
+    this.safeDestroy();
 
     this.connection = joinVoiceChannel({
       channelId: channel.id,
@@ -26,20 +42,38 @@ class MusicPlayer {
       adapterCreator: message.guild.voiceAdapterCreator
     });
 
+    this.player.removeAllListeners();
+
     this.player.on(AudioPlayerStatus.Idle, () => {
-      this.connection.destroy();
+      this.safeDestroy();
     });
 
     this.player.on('error', error => {
       console.error('Player error:', error.message);
-      this.connection.destroy();
+      this.safeDestroy();
     });
 
-    const yt = spawn('yt-dlp', [
+    // Detect if input is URL or search query
+    const isURL = query.startsWith('http://') || query.startsWith('https://');
+
+    const ytArgs = [
       '-f', 'bestaudio[ext=webm]',
-      '-o', '-',
-      url
-    ]);
+      '-o', '-'
+    ];
+
+    if (isURL) {
+      ytArgs.push(query);
+    } else {
+      ytArgs.push(`ytsearch1:${query}`);
+    }
+
+    const yt = spawn('yt-dlp', ytArgs);
+
+    this.currentProcess = yt;
+
+    yt.stderr.on('data', data => {
+      console.log(data.toString());
+    });
 
     const resource = createAudioResource(yt.stdout, {
       inputType: StreamType.WebmOpus
